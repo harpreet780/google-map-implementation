f38b012fbc97a260d181a3f26d82015bd11b2cba
harpreet780 <harpreetsaini276@gmail.com>
2023-05-05 16:57:09 +0530

    implement google map

---
+++ src/Pages/Home.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import { GoogleMap, LoadScript, Marker, useJsApiLoader } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '80%',
+  height: '400px'
+};
+
+const center = {
+  lat: 30.733315,
+  lng: 76.779419
+}
+
+const markerList = [
+  {
+    name: "Chandigarh",
+    lat: 30.733315,
+    lng: 76.779419
+  },
+  {
+    name: "Ludhiana",
+    lat: 30.900965,
+    lng: 75.857277
+  },
+  {
+    name: "Himachal",
+    lat: 31.817261,
+    lng: 77.302757
+  },
+]
+
+const Home = () => {
+  // useJsApiLoader and LoadScript-gave various props but LOADSCRIPT(gave more props than jsApiLoader)
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyDU1M27ycDQ6o-u15AX2SfMbv36Oyec5J4"
+  })
+
+  const [map, setMap] = useState(null)
+
+  const onLoad = (map) => {
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+    setMap(map)
+  }
+
+  const onUnmount = () => {
+    setMap(null)
+  }
+
+  console.log(map, "mappp")
+
+  return (
+    <div className='d-flex justify-content-center p-5'>
+      {
+        isLoaded ? (
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={2}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+          >
+            {markerList.map((places) => {
+              let lat = parseFloat(places.lat, 10);
+              let lng = parseFloat(places.lng, 10);
+
+              return (
+                <Marker
+                  position={{ lat: lat, lng: lng }}
+                >
+                  <div>{places.name}</div>
+                </Marker>
+              )
+            })}
+          </GoogleMap>
+        ) : <></>
+      }
+      {/* or I also used like this without onLoad function */}
+      {/* <LoadScript googleMapsApiKey="AIzaSyDU1M27ycDQ6o-u15AX2SfMbv36Oyec5J4">
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
+          onUnmount={onUnmount}
+        >
+          <></>
+        </GoogleMap >
+      </LoadScript> */}
+    </div>
+  )
+}
+
+export default Home;
